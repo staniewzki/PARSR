07044310c245431b381651a33c9c23c2e4152175
Michał Staniewski <m.staniewski@student.uw.edu.pl>
2024-05-15 15:38:42 +0000

    Initial commit
    Signed-off-by: Ubuntu <st114@st114vm101.rtb-lab.pl>

---
+++ src/main/java/mstan/SimpleClientApplication.java
@@ -0,0 +1,17 @@
+package mstan;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleClientApplication {
+
+    private static final Logger log = LoggerFactory.getLogger(SimpleClientApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleClientApplication.class, args);
+    }
+
+}
